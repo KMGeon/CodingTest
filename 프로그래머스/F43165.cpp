8ce1ec58f98ed06170a2f6733dafaa7997483fda
mugeon <pos04167@kakao.com>
2025-09-04 23:56:00 +0900

    TIL 0904

--- 프로그래머스/F43165.cpp
+++ 프로그래머스/F43165.cpp
@@ -1,30 +1,29 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-int answer = 0;
-
-void dfs(int idx, int sum, vector<int>& numbers, int target) {
-    if (idx == numbers.size()) {
-        if (sum == target) {
-            answer++;
-        }
-        return;
+using namespace std;
+int Findseries(int N)
+{
+    int i = 666;
+    int series = 0;
+    string target;
+    while(1)
+    {
+        target = to_string(i);
+        for(int j = 0; j < target.length()-2; j++)
+            if(target[j] == '6' && target[j+1] == '6' && target[j+2] == '6')
+            {
+                series++;
+                if(series == N)
+                    return i;
+                break;
+            }
+        i++;
     }
-    
-    dfs(idx + 1, sum + numbers[idx], numbers, target);
-    dfs(idx + 1, sum - numbers[idx], numbers, target);
 }
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    vector<int> numbers = {1,1,1,1,1};
-    int target = 3;
-    
-    dfs(0, 0, numbers, target);
-    cout << answer << endl;
-
-    return 0;
+    int N;
+    cin >> N;
+    cout << Findseries(N);
 }
