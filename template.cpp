01d3c7eb4b0f7be5c4aac362dd8641ad12274410
mugeon <pos04167@kakao.com>
2025-09-19 16:33:28 +0900

    TIL 0919

--- template.cpp
+++ template.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+constexpr int SAFE = 1004;
+int n,m;
+int grid[SAFE][SAFE], visited[SAFE][SAFE];
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+
     return 0;
 }
